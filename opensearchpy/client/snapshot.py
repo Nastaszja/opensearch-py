5fab7170487e150eafc32ecd9e2001c53a698bdc
César Roaldès <crs@sahar.fr>
2024-03-07 17:59:18 +0100

    feat SM policy: add start/stop sm policy functions

--- opensearchpy/client/snapshot.py
+++ opensearchpy/client/snapshot.py
@@ -292,7 +292,47 @@ class SnapshotClient(NamespacedClient):
             headers=headers,
             body=body,
         )
-    
+
+    @query_params("master_timeout", "cluster_manager_timeout", "timeout")
+    def stop_policy(self, policy, params=None, headers=None):
+        """
+        Stop a SM policy.
+
+
+        :arg policy: A policy name
+        :arg master_timeout (Deprecated: use cluster_manager_timeout): Explicit operation timeout for connection
+            to master node
+        :arg cluster_manager_timeout: Explicit operation timeout for connection
+            to cluster_manager node
+        :arg timeout: Explicit operation timeout
+        """
+        return self.transport.perform_request(
+            "POST",
+            _make_path("_plugins", "_sm", "policies", policy, "_stop"),
+            params=params,
+            headers=headers,
+        )
+
+    @query_params("master_timeout", "cluster_manager_timeout", "timeout")
+    def start_policy(self, policy, params=None, headers=None):
+        """
+        Start a SM policy.
+
+
+        :arg policy: A policy name
+        :arg master_timeout (Deprecated: use cluster_manager_timeout): Explicit operation timeout for connection
+            to master node
+        :arg cluster_manager_timeout: Explicit operation timeout for connection
+            to cluster_manager node
+        :arg timeout: Explicit operation timeout
+        """
+        return self.transport.perform_request(
+            "POST",
+            _make_path("_plugins", "_sm", "policies", policy, "_start"),
+            params=params,
+            headers=headers,
+        )
+
     @query_params("master_timeout", "cluster_manager_timeout", "wait_for_completion")
     def restore(self, repository, snapshot, body=None, params=None, headers=None):
         """
